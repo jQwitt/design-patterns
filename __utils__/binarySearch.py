33c490906e7d0f40f3f10b730be46f01fbc9bb30
Jack Witt <jwitt@expediagroup.com>
2022-06-23 12:15:57 -0700

    feat: add utils, binSerach

---
+++ __utils__/binarySearch.py
@@ -0,0 +1,32 @@
+debug = False
+sortedNums: list = [1, 2, 3, 4, 5, 6]
+
+'''
+    @return index of target
+'''
+def binSearch(arr: list[int], target: int) -> int:
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+
+        mid = left + (right - left) // 2
+        
+        if debug:
+            print('left: {}'.format(left))
+            print('mid: {}'.format(mid))
+            print('right: {}'.format(right))
+
+        if arr[mid] == target:
+            return mid
+
+        if arr[mid] < target:
+            left = mid + 1
+        elif arr[mid] > target:
+            right = mid - 1
+
+    return -1
+
+for n in sortedNums:
+    print("passed" if binSearch(sortedNums, n) == sortedNums.index(n) else "failed")
+for n in [-1, 100]:
+    print("passed" if binSearch(sortedNums, n) == -1 else "failed")
